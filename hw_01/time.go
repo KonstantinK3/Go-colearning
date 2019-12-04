950745fcc8e2a9c993e013bdb07ac4a7c22e2661
zal1358@gmail.com <zal1358@gmail.com>
2019-12-04 17:22:10 +0200

    exit added

--- hw_01/time.go
+++ hw_01/time.go
@@ -9,13 +9,16 @@ import (
 )
 
 func main() {
-	timeServer := "0.beevik-ntp.pool.ntp.org"
+	timeServer := "0.beevik-ntp.pool.ntp.org1"
 	nTime, err := ntp.Time(timeServer)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Oh my, something bad happened: %d \n", err)
+		os.Exit(1)
 	}
 
 	fmt.Printf("Current time from server %s is: %s \n", timeServer, nTime)
 	fmt.Printf("Local machine time is: %s \n", time.Now())
+
+	os.Exit(0)
 }
