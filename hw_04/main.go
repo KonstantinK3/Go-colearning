4f8566649abd67bdc804b1de8dcf779427592d7a
zal1358@gmail.com <zal1358@gmail.com>
2019-12-15 18:09:45 +0200

    comparator fixes

--- hw_04/main.go
+++ hw_04/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 
-	data := []int{41, 72, 19, 36, 65}
+	data := []int{41, 72, 19, 99, 36, 65}
 
 	result, _ := unimax.FindMax(data, func(i, j int) bool {
 		return data[i] < data[j]
@@ -16,18 +16,18 @@ func main() {
 
 	fmt.Printf("Max value: %v\n", result)
 
-	data1 := []string{"cmon", "baby", "light", "my", "fire"}
+	data1 := []string{"cmon", "baby", "a", "light", "my", "fire", "z"}
 
 	result, _ = unimax.FindMax(data1, func(i, j int) bool {
-		return data[i] < data[j]
+		return data1[i] < data1[j]
 	})
 
 	fmt.Printf("Max value: %v\n", result)
 
-	data2 := []float64{41.1, 72.5, 19.66, 36.0001, 65.2}
+	data2 := []float64{41.1, 72.5, 19.66, 105111, 36.0001, 65.2}
 
 	result, _ = unimax.FindMax(data2, func(i, j int) bool {
-		return data[i] < data[j]
+		return data2[i] < data2[j]
 	})
 
 	fmt.Printf("Max value: %v\n", result)
