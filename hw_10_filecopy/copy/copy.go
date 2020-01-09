fe881c41528b0af85f7d90d1a21ac8c6780886d2
zal1358@gmail.com <zal1358@gmail.com>
2020-01-09 15:24:42 +0200

    full copy

--- hw_10_filecopy/copy/copy.go
+++ hw_10_filecopy/copy/copy.go
@@ -1,15 +1,36 @@
 package copy
 
 import (
-	"errors"
 	"fmt"
+	"io"
+	"os"
 )
 
 //Copy is for copyiing files and stuff. Pretty self-explanatiry
-func Copy(from string, to string, offset int, limit int) error {
-	fmt.Println("started")
+func Copy(src, dst string, offset int, limit int) error {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
 
-	fmt.Println(from, to, limit, offset)
+	if !sourceFileStat.Mode().IsRegular() {
+		return fmt.Errorf("%s is not a regular file", src)
+	}
 
-	return errors.New("Put error here")
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+
+	fmt.Printf("%#v bytes copied", nBytes)
+
+	return err
 }
